b1ba840387888f85b0a40181047173d38b6d161c
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2026-01-25 00:09:52 +0200

    added dummy to product and product details

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Products() {
+export default function ProductDetails() {
   return (
     <div className="max-w-screen-xl mx-auto py-16 px-4">
-      <h1 className="text-3xl font-bold text-white">Products</h1>
+      <h1 className="text-3xl font-bold text-white">Product Details</h1>
       <p className="text-gray-300 mt-4">
-        Browse all our products here.
+        Browse all our product details here.
       </p>
     </div>
   )
---
+++ src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div className="max-w-screen-xl mx-auto py-16 px-4">
+      <h1 className="text-3xl font-bold text-white">Products</h1>
+      <p className="text-gray-300 mt-4">
+        Browse all our products here.
+      </p>
+    </div>
+  )
+}
