dce321cedb9681f11e353021bb106e244373d0ea
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2026-01-25 00:03:43 +0200

    dummy data till develop these components

--- src/app/brands/page.jsx
+++ src/app/brands/page.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Brands() {
   return (
-    <div>
-      
+    <div className="max-w-screen-xl mx-auto py-16 px-4">
+      <h1 className="text-3xl font-bold text-white">Brands</h1>
+      <p className="text-gray-300 mt-4">All our amazing brands listed here.</p>
     </div>
   )
 }
--- src/app/categories/[id]/page.jsx
+++ src/app/categories/[id]/page.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Category() {
   return (
-    <div>
-      
+    <div className="max-w-screen-xl mx-auto py-16 px-4">
+      <h1 className="text-3xl font-bold text-white">Categories</h1>
+      <p className="text-gray-300 mt-4">All our amazing Categories listed here.</p>
     </div>
   )
 }
