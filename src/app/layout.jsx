83066026346e36c7eec1da345a6914ea763648b9
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2026-01-24 23:54:32 +0200

    added the logo for favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthenticationContextProvider>
