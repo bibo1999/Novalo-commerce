dea14b81ed4a262008553f1b6e81f795e6a9be16
Abdelrahman <abdelrahmanmoataz19@gmail.com>
2026-01-25 00:14:24 +0200

    added dummy to wishlist page

---
+++ src/app/wishlist/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Wishlist() {
+  return (
+    <div>
+      <h1>Wishlist</h1>
+      <p>Here all the wishlist you have chosen</p>
+    </div>
+  )
+}
